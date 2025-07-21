d20e024225ee04153776ee42d37338bbfb214e3a
Kazuyuki Shima <shima@hiroshima-cu.ac.jp>
2025-07-21 12:58:56 +0900

    deleted:    cui/ai-chat.sh

--- cui/ai-chat.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dir="$(dirname $0)"
-python3 $dir/ai-chat.py \
-        --history-file chat-history.json \
-        --model gemini-1.5-flash-latest \
-        --api-key AIzaSyBIjhW6tjhrY8fIS18shxDdVQpkCenNtf8
